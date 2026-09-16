931c5af4d4286f37ea62ba5d0e788cf5746c7d94
Guilherme Araujo <guilherme.araujo.fernandes@ccc.ufcg.edu.br>
2026-09-15 22:34:36 -0300

    ajuste na classe analise.py

--- src/python/analise.py
+++ src/python/analise.py
@@ -2,24 +2,32 @@ import pandas as pd
 import plotly.express as px
 
 def executar_analise(arquivo):
-    df = pd.read_csv(arquivo)
+    df_ini = pd.read_csv(arquivo)
 
-    
+    #save values before cleaning and dataframe after cleaning
+    df, nulos, duplicados, linhas, colunas = limpar_dados(df_ini)
+
+    #create "dados"
     dados = {
-        "total_linhas": len(df),
-        "total_colunas": len(df.columns),
-        "valores_nulos": df.isnull().sum().sum(),
-        "valores_duplicados": df.duplicated().sum(),
+        "total_linhas": linhas,
+        "total_colunas": colunas,
+        "valores_nulos": nulos,
+        "valores_duplicados": duplicados,
         "graficos_html": []
     }
-    
+
+    #Select the chart type based on the column type
     for coluna in df.columns:
         tipo = df[coluna].dtype
+
+        #column type numeric
         if pd.api.types.is_numeric_dtype(tipo):
             fig = px.histogram(df, x=coluna, title=f'Distribuição de {coluna}', marginal="box")
+
             html_fig = fig.to_html(full_html=False, include_plotlyjs='cdn')
             dados["graficos_html"].append(html_fig)
 
+        #column type string or object
         elif pd.api.types.is_string_dtype(tipo) or pd.api.types.is_object_dtype(tipo):
             if df[coluna].nunique() < 20:
                 contagem = df[coluna].value_counts().reset_index()
@@ -30,3 +38,20 @@ def executar_analise(arquivo):
                 dados["graficos_html"].append(html_fig)
 
     return dados
+
+def limpar_dados(df):
+    #save values initials
+
+    linhas = len(df)
+    colunas = len(df.columns)
+    nulos = int(df.isnull().sum().sum())
+    duplicadas = int(df.duplicated().sum())
+
+    #cleaning values: duplicates, nulls
+    df.drop_duplicates(inplace=True)
+    df.dropna(axis=1, how='all', inplace=True)
+    df.dropna(axis=0, how='all', inplace=True)
+
+    return df, nulos, duplicadas, linhas, colunas
+
+
