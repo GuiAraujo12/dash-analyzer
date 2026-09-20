63e9f226ecba94b09c206b96bfd2711bb9b4f203
Guilherme Araujo <guilherme.araujo.fernandes@ccc.ufcg.edu.br>
2026-09-20 18:07:50 -0300

    feat: substitui a api key do google pela api key do groq

--- src/python/iaanalise.py
+++ src/python/iaanalise.py
@@ -1,28 +1,47 @@
 import os
 from dotenv import load_dotenv
-from google import genai
-from google.genai import types
+from groq import Groq
+
 
 load_dotenv(override=True)
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-MODEL_NAME = "gemini-3.6-flash"
-config = types.GenerateContentConfig(max_output_tokens=1500)
-chat_session = None
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+MODEL_NAME = "openai/gpt-oss-20b"
+historico = []
 
 def executar_ia(csv_texto):
-    global chat_session
-    chat_session = client.chats.create(model=MODEL_NAME, config=config)
+    global historico
 
     prompt_inicial = (
         f"Aqui estão as 50 primeiras linhas do arquivo CSV para análise:\n{csv_texto}\n\n"
         "Instrução: Seja objetivo. Limite essa análise inicial a 2 parágrafos, e na resposta não fale sobre o número de linhas do csv que estão sendo consideradas para a análise.")
 
-    response = chat_session.send_message(prompt_inicial)
-    return response.text
+    historico = [{"role": "user", "content": prompt_inicial}]
+
+    response = client.chat.completions.create(
+        model=MODEL_NAME,
+        messages=historico,
+        max_tokens=1500,
+        timeout=30,
+    )
+    resposta_texto = response.choices[0].message.content
+    historico.append({"role": "assistant", "content": resposta_texto})
+    return resposta_texto
 
 def enviar_msg(mensagem):
-    if not chat_session:
+    global historico
+    if not historico:
         return "Nenhum chat ativo."
 
-    response = chat_session.send_message(mensagem)
-    return response.text
+    historico.append({"role": "user", "content": mensagem})
+
+    response = client.chat.completions.create(
+        model=MODEL_NAME,
+        messages=historico,
+        max_tokens=1500,
+        timeout=30,
+    )
+
+    resposta_texto = response.choices[0].message.content
+    historico.append({"role": "assistant", "content": resposta_texto})
+    return resposta_texto
